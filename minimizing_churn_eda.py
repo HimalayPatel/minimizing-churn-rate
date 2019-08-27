5c846bd63562f6451a3dc5303b18618ceb219b71
Himalay Patel <himalaypatel@hotmail.com>
2019-08-26 20:58:24 -0700

    Add files via upload

---
+++ minimizing_churn_eda.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataset = pd.read_csv("C:\\Users\\himal\\Desktop\\Machine Learning Practicals\\P4- Minimizing Churn Rate through Analysis of Financial Habits\\P39-Minimizing-Churn-Data\\churn_data.csv") # Users who were 60 days enrolled, churn in the next 30
+
+# EDA
+# print(dataset.head(5))
+# print(dataset.columns)
+# print(dataset.describe())
+# print(dataset.isna().any())
+# print(dataset.isna().sum())
+dataset=dataset[pd.notnull(dataset['age'])]
+dataset = dataset.drop(columns = ['credit_score', 'rewards_earned'])
+
+# Histograms
+# dataset2 = dataset.drop(columns=['user', 'churn','housing','payment_type','zodiac_sign'])
+# fig = plt.figure(figsize=(20, 15))
+# plt.suptitle('Histograms of Numerical Columns', fontsize=20)
+# for i in range(1, dataset2.shape[1] + 1):
+#     plt.subplot(6, 4, i)
+#     f = plt.gca()
+#     f.axes.get_yaxis().set_visible(False)
+#     f.set_title(dataset2.columns.values[i - 1])
+#     vals = np.size(dataset2.iloc[:, i - 1].unique())
+#     plt.hist(dataset2.iloc[:, i - 1], bins=vals, color='#FF0077')
+# fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+# plt.show()
+
+# Pie Charts
+# dataset2 = dataset[['housing', 'is_referred', 'app_downloaded',
+#                     'web_user', 'app_web_user', 'ios_user',
+#                     'android_user', 'registered_phones', 'payment_type',
+#                     'waiting_4_loan', 'cancelled_loan',
+#                     'received_loan', 'rejected_loan', 'zodiac_sign',
+#                     'left_for_two_month_plus', 'left_for_one_month', 'is_referred']]
+# fig = plt.figure(figsize=(15, 12))
+# plt.suptitle('Pie Chart Distributions', fontsize=20)
+# for i in range(1, dataset2.shape[1] + 1):
+#     plt.subplot(5, 4, i)
+#     f = plt.gca()
+#     f.axes.get_yaxis().set_visible(False)
+#     f.set_title(dataset2.columns.values[i - 1])
+#     values = dataset2.iloc[:, i - 1].value_counts(normalize=True).values
+#     index = dataset2.iloc[:, i - 1].value_counts(normalize=True).index
+#     plt.pie(values, labels=index, autopct='%1.1f%%')
+#     plt.axis('equal')
+# fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+# plt.show()
+# Exploring Uneven Features
+# print(dataset[dataset2.waiting_4_loan == 1].churn.value_counts())
+# print(dataset[dataset2.cancelled_loan == 1].churn.value_counts())
+# print(dataset[dataset2.received_loan == 1].churn.value_counts())
+# print(dataset[dataset2.rejected_loan == 1].churn.value_counts())
+# print(dataset[dataset2.left_for_one_month == 1].churn.value_counts())
+
+# Correlation with the Response Variable
+# dataset.drop(columns=['user', 'churn', 'housing', 'payment_type',
+#                       'registered_phones', 'zodiac_sign']).corrwith(dataset.churn).plot.bar(
+#     figsize=(20,10),title = 'Correlation with Response variable',
+#               fontsize = 15, rot = 45,
+#               grid = True)
+# plt.tight_layout()
+# plt.show()
+
+# Correlation Matrix
+sns.set(style="white")
+corr = dataset.drop(columns = ['user', 'churn','housing','payment_type','zodiac_sign']).corr()
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+f, ax = plt.subplots(figsize=(18, 15))
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.suptitle("Correlation Matrix",fontsize=20)
+plt.show()
+
+dataset = dataset.drop(columns = ['app_web_user'])
+# dataset = dataset.drop(columns = ['housing','payment_type','zodiac_sign'])
+# Note: Although there are somewhat correlated fields, they are not colinear
+# These feature are not functions of each other, so they won't break the model
+# But these feature won't help much either. Feature Selection should remove them.
+
+dataset.to_csv('new_churn_data.csv', index = False)
+
---
+++ minimizing_churn_model.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random
+
+dataset = pd.read_csv('new_churn_data.csv')
+
+# Data Preparation
+user_identifier = dataset['user']
+dataset = dataset.drop(columns = ['user'])
+
+# One-Hot Encoding
+# print(dataset.housing.value_counts())
+# print(dataset.payment_type.value_counts())
+# print(dataset.zodiac_sign.value_counts())
+# dataset.groupby('housing')['churn'].nunique().reset_index()
+dataset = pd.get_dummies(dataset)
+# print(dataset.columns)
+dataset = dataset.drop(columns = ['housing_na', 'zodiac_sign_na', 'payment_type_na'])
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(dataset.drop(columns = ['churn']), dataset['churn'],
+                                                    test_size = 0.2,
+                                                    random_state = 0)
+# Balancing the Training Set
+y_train.value_counts()
+pos_index = y_train[y_train.values == 1].index
+neg_index = y_train[y_train.values == 0].index
+
+if len(pos_index) > len(neg_index):
+    higher = pos_index
+    lower = neg_index
+else:
+    higher = neg_index
+    lower = pos_index
+
+random.seed(0)
+higher = np.random.choice(higher, size=len(lower))
+lower = np.asarray(lower)
+new_indexes = np.concatenate((lower, higher))
+
+X_train = X_train.loc[new_indexes,]
+y_train = y_train[new_indexes]
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train2 = pd.DataFrame(sc_X.fit_transform(X_train))
+X_test2 = pd.DataFrame(sc_X.transform(X_test))
+X_train2.columns = X_train.columns.values
+X_test2.columns = X_test.columns.values
+X_train2.index = X_train.index.values
+X_test2.index = X_test.index.values
+X_train = X_train2
+X_test = X_test2
+
+# Fitting Model to the Training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+cm = confusion_matrix(y_test, y_pred)
+# print(accuracy_score(y_test, y_pred))
+# print(precision_score(y_test, y_pred)) # tp / (tp + fp)
+# print(recall_score(y_test, y_pred)) # tp / (tp + fn)
+# print(f1_score(y_test, y_pred))
+
+df_cm = pd.DataFrame(cm, index = (0, 1), columns = (0, 1))
+plt.figure(figsize = (10,7))
+sns.set(font_scale=1.4)
+sns.heatmap(df_cm, annot=True, fmt='g')
+# print("Test Data Accuracy: %0.4f" % accuracy_score(y_test, y_pred))
+# plt.show()
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+# print("K-Fold Cross Validation Accuracy: %0.3f (+/- %0.3f)" % (accuracies.mean(), accuracies.std() * 2))
+
+# Analyzing Coefficients
+temp=pd.concat([pd.DataFrame(X_train.columns, columns = ["features"]),
+           pd.DataFrame(np.transpose(classifier.coef_), columns = ["coef"])
+          ],axis = 1)
+# print(temp)
+
+# Feature Selection
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+
+classifier = LogisticRegression()
+
+rfe = RFE(classifier, 20)
+rfe = rfe.fit(X_train, y_train)
+
+# print(rfe.support_)
+# print(rfe.ranking_)
+# print(X_train.columns[rfe.support_])
+
+# Fitting model using best 20 features
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression()
+classifier.fit(X_train[X_train.columns[rfe.support_]], y_train)
+
+y_pred = classifier.predict(X_test[X_train.columns[rfe.support_]])
+
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+cm = confusion_matrix(y_test, y_pred)
+# print(accuracy_score(y_test, y_pred))
+# print(precision_score(y_test, y_pred)) # tp / (tp + fp)
+# print(recall_score(y_test, y_pred)) # tp / (tp + fn)
+# print(f1_score(y_test, y_pred))
+
+df_cm = pd.DataFrame(cm, index = (1, 0), columns = (1, 0))
+plt.figure(figsize = (10,7))
+sns.set(font_scale=1.4)
+sns.heatmap(df_cm, annot=True, fmt='g')
+# plt.show()
+# print("Test Data Accuracy: %0.4f" % accuracy_score(y_test, y_pred))
+
+temp= pd.concat([pd.DataFrame(X_train[X_train.columns[rfe.support_]].columns, columns = ["features"]),
+           pd.DataFrame(np.transpose(classifier.coef_), columns = ["coef"])
+           ],axis = 1)
+print(temp)
+
+# Formatting final results (user + actual + predicted)
+final_results = pd.concat([y_test, user_identifier], axis = 1).dropna()
+final_results['predicted_churn'] = y_pred
+final_results = final_results[['user', 'churn', 'predicted_churn']].reset_index(drop=True)
+print(final_results)
